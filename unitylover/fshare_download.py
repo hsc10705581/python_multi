bdd9a11997e67c9f27532a89fe7e8a60894e285b
hsc <hsc934936408@sjtu.edu.cn>
2019-03-08 10:43:42 +0800

    almost finished carbing

--- unitylover/fshare_download.py
+++ unitylover/fshare_download.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3.7
-# -*- coding: utf-8 -*-  
+# -*- coding: utf-8 -*-
+from get_fshare import FSAPI
+import unittest
+
+finished = open("finished.txt").readlines()
+bot = FSAPI('utvyoxrk@emlpro.com', 'ahihihi')
+bot.login()
+print(finished[0])
+res = bot.get_file_info(finished[0])
+print(res)
+
+for url in finished:
+    pass
